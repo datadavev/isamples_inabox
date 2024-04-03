c3001e2058479f5554e114a125739cda42d59eef
Danny Mandel <danny.mandel@gmail.com>
2024-04-03 14:45:17 -0700

    Checkpoint, have the ControlledVocabulary working off the uijson generated dicts

--- isamples_metadata/vocabularies/vocabulary_mapper.py
+++ isamples_metadata/vocabularies/vocabulary_mapper.py
@@ -1,7 +1,7 @@
 import os
 import csv
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Any
 
 from isamples_metadata.metadata_constants import METADATA_LABEL, METADATA_IDENTIFIER
 
@@ -29,23 +29,34 @@ class VocabularyTerm(dict):
 
 
 class ControlledVocabulary:
-    def __init__(self, file_path: str, uri_prefix: str):
-        self.uri_prefix = uri_prefix
+    def __init__(self, uijson_dict: dict[str, Any], key_prefix: str):
         self.vocabulary_terms_by_key = {}
         self.vocabulary_terms_by_label = {}
-        with open(file_path, newline="") as csvfile:
-            csvreader = csv.reader(csvfile, delimiter="\t", quoting=csv.QUOTE_NONE)
-            # skip header
-            next(csvreader)
-            for row in csvreader:
-                # The first column is the term key and has a prefix, which we ignore for the purpose of this API,
-                # e.g. mat:anthropogenicmetal -> anthropogenicmetal
-                key = row[0]
-                label = row[1]
-                uri = os.path.join(uri_prefix, key)
-                term = VocabularyTerm(key, label, uri)
-                self.vocabulary_terms_by_key[key] = term
-                self.vocabulary_terms_by_label[label] = term
+        self._process_uijson_dict(uijson_dict, key_prefix)
+
+    def _process_uijson_dict(self, uijson_dict: dict[str, Any], key_prefix: str):
+        for dict_key, value in uijson_dict.items():
+            # structure looks like this:
+            """
+                "https://w3id.org/isample/vocabulary/material/1.0/material":
+                {
+                    "label":
+                    {
+                        "en": "Material"
+                    },
+                    "children":
+                    [            
+            """
+            uri = dict_key
+            label = value.get("label").get("en")
+            last_piece_of_uri = dict_key.rsplit("/", 1)[-1]
+            term_key = f"{key_prefix}:{last_piece_of_uri}"
+            term = VocabularyTerm(term_key, label, uri)
+            self.vocabulary_terms_by_key[term_key] = term
+            self.vocabulary_terms_by_label[label] = label
+            for child in value.get("children"):
+                self._process_uijson_dict(child, key_prefix)
+
 
     def term_for_key(self, key: str) -> VocabularyTerm:
         return self.vocabulary_terms_by_key.get(key, VocabularyTerm(None, key, None))
@@ -54,7 +65,8 @@ class ControlledVocabulary:
         return self.vocabulary_terms_by_label.get(label, VocabularyTerm(None, label, None))
 
 
-parent_dir = Path(__file__).parent
-MATERIAL_TYPE = ControlledVocabulary(os.path.join(parent_dir, "materialType.txt"), "https://w3id.org/isample/vocabulary/material/0.9/")
-SAMPLED_FEATURE = ControlledVocabulary(os.path.join(parent_dir, "sampledfeature.txt"), "https://w3id.org/isample/vocabulary/sampledfeature/0.9")
-SPECIMEN_TYPE = ControlledVocabulary(os.path.join(parent_dir, "specimenType.txt"), "https://w3id.org/isample/vocabulary/specimentype/0.9")
+# parent_dir = Path(__file__).parent
+# MATERIAL_TYPE = ControlledVocabulary(os.path.join(parent_dir, "materialType.txt"), "https://w3id.org/isample/vocabulary/material/0.9/")
+# SAMPLED_FEATURE = ControlledVocabulary(os.path.join(parent_dir, "sampledfeature.txt"), "https://w3id.org/isample/vocabulary/sampledfeature/0.9")
+# SPECIMEN_TYPE = ControlledVocabulary(os.path.join(parent_dir, "specimenType.txt"), "https://w3id.org/isample/vocabulary/specimentype/0.9")
+# print()
--- tests/test_vocabulary_mapper.py
+++ tests/test_vocabulary_mapper.py
@@ -1,14 +1,17 @@
+import json
 from isamples_metadata.vocabularies import vocabulary_mapper
+from isamples_metadata.vocabularies.vocabulary_mapper import ControlledVocabulary
 
 
 def test_controlled_vocabularies():
-    assert vocabulary_mapper.MATERIAL_TYPE is not None
-    assert vocabulary_mapper.SAMPLED_FEATURE is not None
-    assert vocabulary_mapper.SPECIMEN_TYPE is not None
-    past_human_activities = vocabulary_mapper.SAMPLED_FEATURE.term_for_key("sf:pasthumanoccupationsite")
-    assert past_human_activities is not None
-    metadata_dict = past_human_activities.metadata_dict()
-    assert metadata_dict.get("label") is not None
-    assert metadata_dict.get("identifier") is not None
-    past_human_activities_by_label = vocabulary_mapper.SAMPLED_FEATURE.term_for_label("Site of past human activities")
-    assert past_human_activities_by_label is not None
+    sampled_feature_type = "./test_data/controlled_vocabulary_uijson/sampled_feature_type.json"
+    with open(sampled_feature_type) as source_file:
+        sampled_feature_type_uijson = json.load(source_file)
+        sampled_feature_type_vocabulary = ControlledVocabulary(sampled_feature_type_uijson, "sf")
+        past_human_activities = sampled_feature_type_vocabulary.term_for_key("sf:pasthumanoccupationsite")
+        assert past_human_activities is not None
+        metadata_dict = past_human_activities.metadata_dict()
+        assert metadata_dict.get("label") is not None
+        assert metadata_dict.get("identifier") is not None
+        past_human_activities_by_label = sampled_feature_type_vocabulary.term_for_label("Site of past human activities")
+        assert past_human_activities_by_label is not None
